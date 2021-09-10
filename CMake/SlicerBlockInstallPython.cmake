855ce041ce2f222f8f3e26a00323fa271b978bbe
Andras Lasso <lasso@queensu.ca>
2021-09-10 08:05:24 -0400

    BUG: Add missing Python3.dll to the install package
    fixes #5816

--- CMake/SlicerBlockInstallPython.cmake
+++ CMake/SlicerBlockInstallPython.cmake
@@ -79,7 +79,7 @@ To create a Slicer package including python libraries, you can *NOT* provide you
     install(FILES "${PYTHON_LIBRARY_PATH}/${PYTHON_LIB_BASE}.dll"
       DESTINATION bin
       COMPONENT Runtime)
-    # Copy Python3.dll
+    # Copy python3.dll
     # The file name is always exactly this, as this file is guaranteed
     # to be ABI compatible across all Python3 versions
     # (see https://www.python.org/dev/peps/pep-0384/#linkage
